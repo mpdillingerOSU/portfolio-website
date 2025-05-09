88122eb93ba6d3b44c7d098acfb85c5a31dacaa2
mpdillingerOSU <mpdillinger@gmail.com>
2025-05-09 13:07:01 -0500

    add custom selection input component for sorting projects on the project page

---
+++ frontend/src/components/SelectionInput.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { RxCaretDown } from "react-icons/rx";
+
+function SelectionInput({options, value, onChange}) {
+    const [displayOptions, setDisplayOptions] = useState(false);
+    const [selection, setSelection] = useState(value);
+
+    const updateOptionsVisibility = (val) => {
+        setDisplayOptions(val);
+    }
+
+    const updateSelection = (option) => {
+        setSelection(option);
+        onChange(option[0]);
+    }
+
+    const optionsRef = useRef(null);
+    //Check of selection ref also ensures that we don't get an open and close double effect when clicking on the actual selection box
+    const selectionRef = useRef(null);
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if(optionsRef.current && !optionsRef.current.contains(event.target)
+                && selectionRef.current && !selectionRef.current.contains(event.target)) {
+                updateOptionsVisibility(false);
+            }
+        }
+
+        //Bind event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        
+        return () => {
+            //Unbind event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div class="selection-input-container" onClick={() => updateOptionsVisibility(!displayOptions)}>
+            <button class="selection-input" ref={selectionRef}>
+                <span>
+                    {selection !== undefined ? selection[1] : "Select an Option"}
+                </span>
+                <RxCaretDown />
+            </button>
+            <div class={"selection-options-container" + (displayOptions ? " selection-options-container-displayed" : "")} ref={optionsRef}>
+                {Array.from({ length: options.length }, (_, i) => (
+                    <button className="selection-option" onClick={() => updateSelection(options[i])}>{options[i][1]}</button>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default SelectionInput;
--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -1,18 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { highlightNavButton } from '../scripts/NavigationActions';
 import { projectList } from '../data/ProjectData';
 import ProjectContainer from '../components/ProjectContainer';
+import SelectionInput from '../components/SelectionInput';
 
 function ProjectsPage() {
+    const [sortType, setSortType] = useState("featured");
+    const [projects, setProjects] = useState([...projectList]);
+
     useEffect(() => {
         highlightNavButton("projects");
     }, []);
 
+    const attributeCount = (project, attribute) => {
+        const arr = [];
+        for(let subproject of project.subprojects){
+            for(let val of subproject[attribute]){
+                if(!arr.includes(val)){
+                    arr.push(val);
+                }
+            }
+        }
+
+        return arr.length;
+    }
+
+    const updateSortType = (type) => {
+        setSortType(type);
+
+        if(type === "featured"){
+            projects.sort((a, b) => a.index - b.index);
+        } else if(type === "alphabetic"){
+            projects.sort((a, b) => a.name.localeCompare(b.name));
+        } else if(type === "languages"){
+            projects.sort((a, b) => attributeCount(b, "languages") - attributeCount(a, "languages"));
+        } else if(type === "skills"){
+            projects.sort((a, b) => attributeCount(b, "skills") - attributeCount(a, "skills"));
+        }
+    }
+
     return (
         <div id="projects-page" className="page">
+            <div className="filters-section">
+                <div className="input-container">
+                    <span className="input-lead-text">Sort</span>
+                    <SelectionInput options={[["featured", "Featured"], ["alphabetic", "Alphabetic"], ["languages", "# of Languages"], ["skills", "# of Skills"]]} value={["featured", "Featured"]} onChange={(val) => updateSortType(val)} />
+                </div>
+            </div>
             <div className="projects-list">
-                {Array.from({ length: projectList.length }, (_, i) => (
-                    <ProjectContainer projectID={projectList[i].projectID} />
+                {Array.from({ length: projects.length }, (_, i) => (
+                    <ProjectContainer projectID={projects[i].projectID} />
                 ))}
             </div>
         </div>
