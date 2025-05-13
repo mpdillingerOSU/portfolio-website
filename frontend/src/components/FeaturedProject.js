7224596b6b1d5247ae4cbba2b9c9e47d52321ecb
mpdillingerOSU <mpdillinger@gmail.com>
2025-05-13 18:22:00 -0500

    add routing to projects via clicking on featured project on welcome page

--- frontend/src/components/FeaturedProject.js
+++ frontend/src/components/FeaturedProject.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { projectDict } from '../data/ProjectData';
+import { useNavigate } from 'react-router-dom';
 
 function FeaturedProject({projectID}) {
+    const navigate = useNavigate();
+
+    const toProject = (projectID) => {
+        navigate(`/projects/${projectID}`);
+    }
+
     return (
         <div className="featured-project">
-            <div className="outer-hexagon">
+            <div className="outer-hexagon" onClick={(e) => toProject(projectID)}>
                 <div className="inner-hexagon">
                     <img className="featured-project-logo" src={projectDict[projectID].logo} alt={projectDict[projectID].logoAlt} />
                 </div>
