7dc235dd20b9caafc3bc20a366b248d0de30b2f4
mpdillingerOSU <mpdillinger@gmail.com>
2025-05-14 09:55:01 -0500

    add subproject overviews to project page

--- frontend/src/pages/ProjectPage.js
+++ frontend/src/pages/ProjectPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { highlightNavButton } from '../scripts/NavigationActions';
 import { useLocation } from 'react-router-dom';
 import { projectDict } from '../data/ProjectData';
+import LanguageButton from '../components/LanguageButton';
 
 function ProjectPage() {
     const splitPath = useLocation().pathname.split("/");
@@ -19,6 +20,51 @@ function ProjectPage() {
                 <div className="project-page-header-title">
                     {project.name}
                 </div>
+            </div>
+            <div className="subprojects-column">
+                {Array.from({ length: project.subprojects.length }, (_, i) => (
+                    <div className="subproject-section">
+                        <div className="subproject-header">
+                            {project.subprojects[i].name}
+                        </div>
+                        <div className="subproject-overview">
+                            <div className="subproject-details">
+                                <div className="subproject-details-lead-text">
+                                    Skills
+                                </div>
+                                <div className="subproject-details-row">
+                                    {Array.from({ length: project.subprojects[i].skills.length }, (_, j) => (
+                                        <div className="project-skill">
+                                            {project.subprojects[i].skills[j]}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="subproject-details">
+                                <div className="subproject-details-lead-text">
+                                    Languages
+                                </div>
+                                <div className="subproject-details-row">
+                                    {Array.from({ length: project.subprojects[i].languages.length }, (_, j) => (
+                                        <LanguageButton language={project.subprojects[i].languages[j]}/>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="subproject-urls">
+                                {Array.from({ length: project.subprojects[i].urls.length }, (_, j) => (
+                                    <div className="subproject-url-row">
+                                        <div className="subproject-url-header">
+                                            {project.subprojects[i].urls[j][0]}:
+                                        </div>
+                                        <a className="subproject-url-link" href={project.subprojects[i].urls[j][1]} target="_blank" rel="noopener noreferrer">
+                                            {project.subprojects[i].urls[j][1]}
+                                        </a>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
