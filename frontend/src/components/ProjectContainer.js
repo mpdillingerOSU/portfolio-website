7234117a985fa32b23b928dcf9dcabf671c8c212
mpdillingerOSU <mpdillinger@gmail.com>
2025-05-15 09:20:52 -0500

    add technologies to project container

--- frontend/src/components/ProjectContainer.js
+++ frontend/src/components/ProjectContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { projectDict } from '../data/ProjectData';
 import LanguageButton from './LanguageButton';
 import { useNavigate } from 'react-router-dom';
+import TechnologyButton from './TechnologyButton';
 
 function ProjectContainer({projectID}) {
     const navigate = useNavigate();
@@ -10,6 +11,7 @@ function ProjectContainer({projectID}) {
 
     const projectLanguages = [];
     const projectSkills = [];
+    const projectTechnologies = [];
     for(let subproject of project.subprojects){
         for(let skill of subproject.skills){
             if(!projectSkills.includes(skill)){
@@ -20,6 +22,12 @@ function ProjectContainer({projectID}) {
         for(let language of subproject.languages){
             if(!projectLanguages.includes(language)){
                 projectLanguages.push(language);
+            }
+        }
+
+        for(let technology of subproject.technologies){
+            if(!projectTechnologies.includes(technology)){
+                projectTechnologies.push(technology);
             }
         }
     }
@@ -33,17 +41,24 @@ function ProjectContainer({projectID}) {
             <img className="project-container-logo" src={project.logo} alt={project.logoAlt} />
             <div className="project-container-info">
                 <span className="project-container-name">{project.name}</span>
-                <div className="project-skills-row">
+                <div className="project-features-row">
                     {Array.from({ length: projectSkills.length }, (_, i) => (
                         <div className="project-skill">
                             {projectSkills[i]}
                         </div>
                     ))}
                 </div>
-                <div className="project-languages-row">
-                    {Array.from({ length: projectLanguages.length }, (_, i) => (
-                        <LanguageButton language={projectLanguages[i]}/>
-                    ))}
+                <div className="project-container-split-row">
+                    <div className="project-features-row">
+                        {Array.from({ length: projectLanguages.length }, (_, i) => (
+                            <LanguageButton language={projectLanguages[i]}/>
+                        ))}
+                    </div>
+                    <div className="project-features-row">
+                        {Array.from({ length: projectTechnologies.length }, (_, i) => (
+                            <TechnologyButton technology={projectTechnologies[i]}/>
+                        ))}
+                    </div>
                 </div>
             </div>
         </button>
