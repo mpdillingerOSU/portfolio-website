b4bb10af37499d740bee84e274dd5f197831ff89
mpdillingerOSU <mpdillinger@gmail.com>
2025-05-27 15:52:57 -0500

    update abstract of ptcgp simulator analysis website project

--- frontend/src/data/ProjectData.js
+++ frontend/src/data/ProjectData.js
@@ -399,7 +399,7 @@ export const projectDict = {
         logo: require("../images/project-logos/ptcgp-sim-logo.png"),
         logoAlt: "pokemon trading card game simulator analysis website",
         version: "v0.0.0-alpha",
-        abstract: "The Analysis Website for the PTCGP Simulator project was created in order to share and analyze the data that was obtained from the simulations made by the PTCGP Simulator project- which can also be found in this portfolio. In particular, the ethics related to the ability for users to obtain all standard cards was given heavy emphasize - both as a means to ensure from a business perspective that users were retained, and from a consumer perspective in order to maximize product enjoyability.",
+        abstract: "The Analysis Website for the PTCGP Simulator project was created in order to share and analyze the data that was obtained from the simulations made by the PTCGP Simulator project - which can also be found in this portfolio. In particular, the ethics related to the ability for users to obtain all standard cards was given heavy emphasize - both as a means to ensure from a business perspective that users were retained, and from a consumer perspective in order to maximize product enjoyability.",
         urls: [
             ["Live Demo", ""],
             ["GitHub", "https://github.com/mpdillingerOSU/ptcgp-simulator-website"]
