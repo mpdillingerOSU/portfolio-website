e06cee9dcbfd5cec1f46f8943290d2906e32c3e6
mpdillingerOSU <mpdillinger@gmail.com>
2025-06-02 17:16:29 -0500

    ensure that the featured projects on the welcome page are always vertically centered on screen for applicable heights

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -12,107 +12,109 @@ function WelcomePage() {
 
     return (
         <div id="welcome-page" className="page">
-            <div className="featured-projects-header">
-                <span>Featured Projects</span>
-                <div className="featured-projects-header-top-left-corner" />
-                <div className="featured-projects-header-bottom-right-corner" />
-            </div>
-            <div className="featured-projects-container wide-featured-projects-container">
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[0].projectID} />
-                    <FeaturedProject projectID={projectList[1].projectID} />
-                    <FeaturedProject projectID={projectList[2].projectID} />
-                    <FeaturedProject projectID={projectList[3].projectID} />
+            <div className="welcome-page-featured-projects-section">
+                <div className="featured-projects-header">
+                    <span>Featured Projects</span>
+                    <div className="featured-projects-header-top-left-corner" />
+                    <div className="featured-projects-header-bottom-right-corner" />
                 </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[4].projectID} />
-                    <FeaturedProject projectID={projectList[5].projectID} />
-                    <FeaturedProject projectID={projectList[6].projectID} />
+                <div className="featured-projects-container wide-featured-projects-container">
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[0].projectID} />
+                        <FeaturedProject projectID={projectList[1].projectID} />
+                        <FeaturedProject projectID={projectList[2].projectID} />
+                        <FeaturedProject projectID={projectList[3].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[4].projectID} />
+                        <FeaturedProject projectID={projectList[5].projectID} />
+                        <FeaturedProject projectID={projectList[6].projectID} />
+                    </div>
                 </div>
-            </div>
-            <div className="featured-projects-container medium-featured-projects-container">
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[0].projectID} />
-                    <FeaturedProject projectID={projectList[1].projectID} />
+                <div className="featured-projects-container medium-featured-projects-container">
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[0].projectID} />
+                        <FeaturedProject projectID={projectList[1].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[2].projectID} />
+                        <FeaturedProject projectID={projectList[3].projectID} />
+                        <FeaturedProject projectID={projectList[4].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[5].projectID} />
+                        <FeaturedProject projectID={projectList[6].projectID} />
+                    </div>
                 </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[2].projectID} />
-                    <FeaturedProject projectID={projectList[3].projectID} />
-                    <FeaturedProject projectID={projectList[4].projectID} />
+                <div className="featured-projects-container small-featured-projects-container">
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[0].projectID} />
+                        <FeaturedProject projectID={projectList[1].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
+                    <div className="featured-projects-row">
+                        <div className="featured-projects-row-buffer" />
+                        <FeaturedProject projectID={projectList[2].projectID} />
+                        <FeaturedProject projectID={projectList[3].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[4].projectID} />
+                        <FeaturedProject projectID={projectList[5].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[6].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
                 </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[5].projectID} />
-                    <FeaturedProject projectID={projectList[6].projectID} />
+                <div className="featured-projects-container smaller-featured-projects-container">
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[0].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[1].projectID} />
+                        <FeaturedProject projectID={projectList[2].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[3].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[4].projectID} />
+                        <FeaturedProject projectID={projectList[5].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[6].projectID} />
+                    </div>
                 </div>
-            </div>
-            <div className="featured-projects-container small-featured-projects-container">
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[0].projectID} />
-                    <FeaturedProject projectID={projectList[1].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-                <div className="featured-projects-row">
-                    <div className="featured-projects-row-buffer" />
-                    <FeaturedProject projectID={projectList[2].projectID} />
-                    <FeaturedProject projectID={projectList[3].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[4].projectID} />
-                    <FeaturedProject projectID={projectList[5].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[6].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-            </div>
-            <div className="featured-projects-container smaller-featured-projects-container">
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[0].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[1].projectID} />
-                    <FeaturedProject projectID={projectList[2].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[3].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[4].projectID} />
-                    <FeaturedProject projectID={projectList[5].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[6].projectID} />
-                </div>
-            </div>
-            <div className="featured-projects-container smallest-featured-projects-container">
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[0].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-                <div className="featured-projects-row">
-                    <div className="featured-projects-row-buffer" />
-                    <FeaturedProject projectID={projectList[1].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[2].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-                <div className="featured-projects-row">
-                    <div className="featured-projects-row-buffer" />
-                    <FeaturedProject projectID={projectList[3].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[4].projectID} />
-                    <div className="featured-projects-row-buffer" />
-                </div>
-                <div className="featured-projects-row">
-                    <div className="featured-projects-row-buffer" />
-                    <FeaturedProject projectID={projectList[5].projectID} />
-                </div>
-                <div className="featured-projects-row">
-                    <FeaturedProject projectID={projectList[6].projectID} />
-                    <div className="featured-projects-row-buffer" />
+                <div className="featured-projects-container smallest-featured-projects-container">
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[0].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
+                    <div className="featured-projects-row">
+                        <div className="featured-projects-row-buffer" />
+                        <FeaturedProject projectID={projectList[1].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[2].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
+                    <div className="featured-projects-row">
+                        <div className="featured-projects-row-buffer" />
+                        <FeaturedProject projectID={projectList[3].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[4].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
+                    <div className="featured-projects-row">
+                        <div className="featured-projects-row-buffer" />
+                        <FeaturedProject projectID={projectList[5].projectID} />
+                    </div>
+                    <div className="featured-projects-row">
+                        <FeaturedProject projectID={projectList[6].projectID} />
+                        <div className="featured-projects-row-buffer" />
+                    </div>
                 </div>
             </div>
             <AppFooter />
