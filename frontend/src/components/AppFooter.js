ffc61ea4c2ffe50f079d9df6028db5934cb8c8fa
mpdillingerOSU <mpdillinger@gmail.com>
2025-06-02 17:00:41 -0500

    add footer buffer to ensure the footer is at the bottom of the page on small content height

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function AppFooter() {
     return (
-        <footer className="app-footer">
-            <div className="app-footer-copyright">&copy; {new Date().getFullYear()} Michael Dillinger. All rights reserved.</div>
-        </footer>
+        <>
+            <div className="app-footer-buffer" />
+            <footer className="app-footer">
+                <div className="app-footer-copyright">&copy; {new Date().getFullYear()} Michael Dillinger. All rights reserved.</div>
+            </footer>
+        </>
     );
 }
 
