7f0fe5874e50582d86dfec4578d6c4b312eea6f8
mpdillingerOSU <mpdillinger@gmail.com>
2025-06-13 14:07:53 -0500

    change enabled featured project main element to link component

--- frontend/src/components/FeaturedProject.js
+++ frontend/src/components/FeaturedProject.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { projectDict } from '../data/ProjectData';
-import { useNavigate } from 'react-router-dom';
 import ProjectHexagon from './ProjectHexagon';
 
 function FeaturedProject({projectID}) {
-    const navigate = useNavigate();
-
-    const toProject = (projectID) => {
-        navigate(`/projects/${projectID}`);
-    }
-
     return (
         <div className="featured-project">
-            <ProjectHexagon projectID={projectID} onClick={(e) => toProject(projectID)} />
+            <ProjectHexagon projectID={projectID} isLink={true} />
             <div className="featured-project-name-outer-container">
                 <div className="featured-project-name-inner-container">
                     {projectDict[projectID].name}
                 </div>
             </div>
         </div>
-  );
+    );
 }
 
 export default FeaturedProject;
--- frontend/src/components/ProjectHexagon.js
+++ frontend/src/components/ProjectHexagon.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { projectDict } from '../data/ProjectData';
+import { Link } from 'react-router-dom';
 
-function ProjectHexagon({projectID, width, borderWidth, onClick}) {
+function ProjectHexagon({projectID, width, borderWidth, isLink}) {
     return (
-        <div className={"outer-hexagon" + (onClick !== undefined ? " hexagon-button" : "")} style={{width: width ?? "180px"}} onClick={(e) => {if(onClick !== undefined){onClick(e);}}}>
-            <div className="inner-hexagon" style={{width: `calc(100% - ${(borderWidth ?? 6) * 2}px)`}}>
-                <img className="featured-project-logo" src={projectDict[projectID].logo} alt={projectDict[projectID].logoAlt} />
-            </div>
-        </div>
+        <>
+            {isLink ? (
+                <Link className="outer-hexagon enabled-hexagon-button" style={{width: width ?? "180px"}} to={`/projects/${projectID}`}>
+                    <div className="inner-hexagon" style={{width: `calc(100% - ${(borderWidth ?? 6) * 2}px)`}}>
+                        <img className="featured-project-logo" src={projectDict[projectID].logo} alt={projectDict[projectID].logoAlt} />
+                    </div>
+                </Link>
+            ) : (
+                <div className="outer-hexagon disabled-hexagon-button" style={{width: width ?? "180px"}}>
+                    <div className="inner-hexagon" style={{width: `calc(100% - ${(borderWidth ?? 6) * 2}px)`}}>
+                        <img className="featured-project-logo" src={projectDict[projectID].logo} alt={projectDict[projectID].logoAlt} />
+                    </div>
+                </div>
+            )}
+        </>
   );
 }
 
